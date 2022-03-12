0b59de7b23b2a115bd75c20f66acee767ccae428
vukdjedovic <vukdjedovic98@gmail.com>
2022-03-12 21:07:54 +0100

    Metode iz interefejsa MagacinInterfejs su implementirane u klasi Magacin pri cemu su tela ovih metoda prazna

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -2,10 +2,57 @@ package magacin;
 
 import java.util.LinkedList;
 
-public class Magacin {
+import magacin.interfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs {
 	
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
 	private LinkedList<KucnaHemija> kucneHemije = new LinkedList<KucnaHemija>();
 	private LinkedList<KancelarijskiMaterijal> kancelarijskiMaterijali = new LinkedList<KancelarijskiMaterijal>();
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Knjiga pronadjiIVratiKnjigu(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void dodajKancelarijskiMaterijal(KancelarijskiMaterijal materijal) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void obrisiKancelarijskiMaterijal(KancelarijskiMaterijal materijal) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public KancelarijskiMaterijal pronadjiIVratiKancMaterijal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void dodajKucnuHemiju(KucnaHemija hemija) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void obrisiKucnuHemiju(KucnaHemija hemija) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public KucnaHemija pronadjiIVratiKucnuHemiju(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
